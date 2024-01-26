d0d2d082660dbbe1b75d532000af386ec5309623
Peter Kofler <peter.kofler@code-cop.org>
2024-01-26 13:43:47 +0100

    Documenting.

--- Anagrams_20240126_TypeScript/tests/anagrams.spec.ts
+++ Anagrams_20240126_TypeScript/tests/anagrams.spec.ts
@@ -2,8 +2,8 @@
 // 2. try to improve the performance of your solution
 
 // see https://stackoverflow.com/a/61129812
-const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] as const;
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] as const;
 type Letters = typeof letters[number];
 
 function isLetter(c: any): c is Letters {
@@ -46,9 +46,16 @@ describe('Anagram', () => {
 
         it('of all letters', () => {
             expect(histogram('abcdefghijklmnopqrstuvwxyz')).
-                toEqual({ a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1, i: 1, 
-                          j: 1, k: 1, l: 1, m: 1, n: 1, o: 1, p: 1, q: 1, r: 1,
-                          s: 1, t: 1, u: 1, v: 1, w: 1, x: 1, y: 1, z: 1 });
+                toEqual({
+                    a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1, i: 1,
+                    j: 1, k: 1, l: 1, m: 1, n: 1, o: 1, p: 1, q: 1, r: 1,
+                    s: 1, t: 1, u: 1, v: 1, w: 1, x: 1, y: 1, z: 1
+                });
+        });
+
+        it('of "documenting"', () => {
+            expect(histogram('documenting')).
+                toEqual({ d: 1, o: 1, c: 1, u: 1, m: 1, e: 1, n: 2, t: 1, i: 1, g: 1 })
         });
     });
 
