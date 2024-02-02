1966ff5ac9cec9f9fbf129c9e7cfbbfc37e1c06d
Gregor Riegler <rieglerg85@gmail.com>
2024-02-02 13:52:20 +0000

    two kinds of parallel change

--- TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/Play.java
+++ TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/Play.java
@@ -18,12 +18,19 @@ public class Play {
         }
     }
 
-    
     public String name;
     public String type;
+    public final Type type2;
 
     public Play(String name, String type) {
         this.name = name;
+        this.type2 = Type.valueOf(name.toUpperCase());
         this.type = type;
+    }
+
+    public Play(String name, Type nameEnum) {
+        this.name = name;
+        this.type2 = nameEnum;
+        this.type = nameEnum.toString();
     }
 }
