1e5d88830c055ed92848584fe486ec10cf6a7db5
Gregor Riegler <rieglerg85@gmail.com>
2024-02-02 13:49:01 +0000

    Add Type Enum

--- TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/Play.java
+++ TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/Play.java
@@ -2,6 +2,23 @@ package theatricalplays;
 
 public class Play {
 
+    enum Type {
+        TRAGEDY("tragedy"), 
+        COMEDY("comedy");
+
+        private String name;
+
+        Type(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    
     public String name;
     public String type;
 
