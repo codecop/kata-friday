2fa06c55b3b063491ca6f1903a7329a967cba074
Peter Kofler <peter.kofler@code-cop.org>
2024-02-16 16:52:13 +0100

    Green.

--- TicTacToe_20240216_Java/src/main/java/Game.java
+++ TicTacToe_20240216_Java/src/main/java/Game.java
@@ -1,21 +1,22 @@
 import java.util.EnumMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class Game {
 
-    EnumMap<Move, Player> field = new EnumMap<>(Move.class);
+    private final Map<Move, Player> field = new EnumMap<>(Move.class);
 
     public void move(Player player, Move move) {
         field.put(move, player);
     }
 
     public Optional<Player> winner() {
-        if (
-                field.get(Move.ROW_A_COLUMN_1) ==
-                        field.get(Move.ROW_A_COLUMN_2) &&
-                        field.get(Move.ROW_A_COLUMN_2) ==
-                                field.get(Move.ROW_A_COLUMN_3)
-        ) {
+        if (field.get(Move.ROW_A_COLUMN_1) == field.get(Move.ROW_A_COLUMN_2) && //
+            field.get(Move.ROW_A_COLUMN_2) == field.get(Move.ROW_A_COLUMN_3)) {
+            return Optional.of(field.get(Move.ROW_A_COLUMN_1));
+        }
+        if (field.get(Move.ROW_A_COLUMN_1) == field.get(Move.ROW_B_COLUMN_1) && //
+                field.get(Move.ROW_B_COLUMN_1) == field.get(Move.ROW_C_COLUMN_1)) {
             return Optional.of(field.get(Move.ROW_A_COLUMN_1));
         }
         return Optional.empty();
