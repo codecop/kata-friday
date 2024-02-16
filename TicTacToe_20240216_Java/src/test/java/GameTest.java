6dcd09c406b0de93bfb6aa75a5edfd5d72870edf
Peter Kofler <peter.kofler@code-cop.org>
2024-02-16 17:24:53 +0100

    Remove duplication in setup.

--- TicTacToe_20240216_Java/src/test/java/GameTest.java
+++ TicTacToe_20240216_Java/src/test/java/GameTest.java
@@ -1,65 +1,65 @@
-import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class  GameTest {
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    Game game = new Game();
 
     @Test
     void shouldDetectWinnerXInRowA() {
-        Game game = new Game();
         game.move(Player.X, Field.ROW_A_COLUMN_1);
         game.move(Player.X, Field.ROW_A_COLUMN_2);
         game.move(Player.X, Field.ROW_A_COLUMN_3);
-        Optional<Player> winner = game.winner();
-        assertWinner(Player.X, game);
+
+        assertWinner(Player.X);
     }
 
     @Test
     void shouldDetectNoWinner() {
-        Game game = new Game();
         game.move(Player.X, Field.ROW_A_COLUMN_1);
         game.move(Player.O, Field.ROW_A_COLUMN_2);
         game.move(Player.X, Field.ROW_A_COLUMN_3);
         Optional<Player> winner = game.winner();
+
         assertFalse(winner.isPresent());
     }
 
     @Test
     void shouldDetectWinnerOInRowA() {
-        Game game = new Game();
         game.move(Player.O, Field.ROW_A_COLUMN_1);
         game.move(Player.O, Field.ROW_A_COLUMN_2);
         game.move(Player.O, Field.ROW_A_COLUMN_3);
-        assertWinner(Player.O, game);
-    }
 
+        assertWinner(Player.O);
+    }
 
     @Test
     void shouldDetectWinnerInColumn1() {
-        Game game = new Game();
         game.move(Player.O, Field.ROW_A_COLUMN_1);
         game.move(Player.O, Field.ROW_B_COLUMN_1);
         game.move(Player.O, Field.ROW_C_COLUMN_1);
-        assertWinner(Player.O, game);
+
+        assertWinner(Player.O);
     }
 
     @Test
     void shouldDetectWinnerXInDiagonal() {
-        Game game = new Game();
         game.move(Player.X, Field.ROW_A_COLUMN_1);
         game.move(Player.X, Field.ROW_B_COLUMN_2);
         game.move(Player.X, Field.ROW_C_COLUMN_3);
-        assertWinner(Player.X, game);
+
+        assertWinner(Player.X);
     }
 
-    private void assertWinner(Player player, Game game) {
-        Optional<Player> actual = game.winner();
-        assertEquals(player.toString(), actual. //
-                map(Player::toString). //
-                orElseGet(() -> "No Winner"));
+    private void assertWinner(Player expected) {
+        String actual = game.winner(). // 
+                map(Player::toString). // hack to have error massage
+                orElseGet(() -> "No Winner");
+        assertEquals(expected.toString(), actual);
     }
 
 }
