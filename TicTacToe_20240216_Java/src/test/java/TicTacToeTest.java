42c482e4261e5bbad7ca4b666239e72d655eec9c
Peter Kofler <peter.kofler@code-cop.org>
2024-02-16 16:33:07 +0100

    Red: shouldDetectWinnerThreeInARowO

--- TicTacToe_20240216_Java/src/test/java/TicTacToeTest.java
+++ TicTacToe_20240216_Java/src/test/java/TicTacToeTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 class TicTacToeTest {
 
     @Test
-    void shouldDetectThreeInARow() {
+    void shouldDetectWinnerThreeInARowX() {
         Game game = new Game();
         game.move(Player.X, Move.A1);
         game.move(Player.X, Move.A2);
         game.move(Player.X, Move.A3);
         Optional<Player> winner = game.winner();
-        assertEquals(Player.X, winner.get());
+        assertWinner(Player.X, winner);
     }
 
     @Test
@@ -25,6 +25,26 @@ class TicTacToeTest {
         game.move(Player.X, Move.A3);
         Optional<Player> winner = game.winner();
         assertFalse(winner.isPresent());
+    }
+
+    @Test
+    void shouldDetectWinnerThreeInARowO() {
+        Game game = new Game();
+        game.move(Player.O, Move.A1);
+        game.move(Player.O, Move.A2);
+        game.move(Player.O, Move.A3);
+        Optional<Player> winner = game.winner();
+        assertWinner(Player.O, winner);
+    }
+
+    private void assertWinner(Player expected, Optional<Player> actual) {
+        assertEquals(expected.toString(), extractWinner(actual));
+    }
+
+    private String extractWinner(Optional<Player> winner) {
+        return winner. //
+                map(Player::toString). //
+                orElseGet(() -> "No Winner");
     }
 
 }
