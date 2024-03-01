7c1f05de8add6c75e662e6ed4ca395aa3177a220
Peter Kofler <peter.kofler@code-cop.org>
2024-03-01 15:04:27 +0100

    Create model for output data.

---
+++ TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/StatementLine.java
@@ -0,0 +1,13 @@
+package theatricalplays;
+
+public class StatementLine {
+    public final String name;
+    public final int audience;
+    public final int amount;
+
+    public StatementLine(String name, int audience, int amount) {
+        this.name = name;
+        this.audience = audience;
+        this.amount = amount;
+    }
+}
--- TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/StatementPrinter.java
+++ TheatricalPlayers_20240202_Java/src/main/java/theatricalplays/StatementPrinter.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+    
     public String print(Invoice invoice, Map<String, Play> plays) {
         List<Performance> performances = invoice.performances;
 
@@ -14,18 +16,25 @@ public class StatementPrinter {
 
         var totalAmount = totalAmountFor(performances, plays);
 
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
         var result = String.format("Statement for %s\n", invoice.customer);
         for (var perf : performances) {
             var play = plays.get(perf.playID);
-
-            // print line for this order
             var thisAmount = perf.amount(play.type);
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            String name = play.name;
+            int audience = perf.audience;
+
+            result += format(new StatementLine(name, audience, thisAmount));
         }
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
+    }
+
+    private String format(StatementLine line) {
+        return String.format("  %s: %s (%s seats)\n", //
+                line.name, //
+                frmt.format(line.amount / 100), //
+                line.audience);
     }
 
     private int totalAmountFor(List<Performance> performances, Map<String, Play> plays) {
