638140b2138cdaba649a229d410aebeaa956a28f
Peter Kofler <peter.kofler@code-cop.org>
2024-04-19 13:54:15 +0200

    Point to my local key.

--- FlagFootball_20240419_MonkeyC/build.bat
+++ FlagFootball_20240419_MonkeyC/build.bat
@@ -1,4 +1,4 @@
 @call clean.bat
-call monkeyc -f monkey.jungle -o bin\Katas.prg -y ..\developer_key -d vivoactive5_sim -w -l 3 %*
+call monkeyc -f monkey.jungle -o bin\Katas.prg -y E:\Develop\Monkey_C\developer_key -d vivoactive5_sim -w -l 3 %*
 @rem -w ... warnings
 @rem -l 3 ... strict type checker
