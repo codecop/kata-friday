a29f45c67b1c23372b07270ae751f867e6db11c8
btvKmf <btvKmf@cyber-dojo.org>
2024-04-26 11:53:45 +0000

    26 ran tests, no prediction, got green

--- GameOfLife_20240426_C#/GoLTest.cs
+++ GameOfLife_20240426_C#/GoLTest.cs
@@ -58,3 +58,41 @@ public class Grid {
 public interface GridSpace {}
 public class EmptySpace : GridSpace {}
 public class AliveCell : GridSpace {}
+
+public class Position
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Position other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked // Overflow is fine, just wrap
+        {
+            int hash = 17;
+            hash = hash * 23 + X.GetHashCode();
+            hash = hash * 23 + Y.GetHashCode();
+            return hash;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Position(X: {X}, Y: {Y})";
+    }
+}
+
