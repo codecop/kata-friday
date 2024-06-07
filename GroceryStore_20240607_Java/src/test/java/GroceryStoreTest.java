e9e661aba5067be8fe9ff9fe6391e08a856851e0
Peter Kofler <peter.kofler@code-cop.org>
2024-06-07 11:15:37 +0200

    grandTotalOfSingleFile

--- GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
+++ GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
@@ -1,9 +1,15 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 class GroceryStoreTest {
 
+    // req 1)
     @Test
     void grandTotalOfEmptyFile() {
         int grandTotal = calculateGrandTotal("\n");
@@ -22,10 +28,27 @@ class GroceryStoreTest {
         assertEquals(5, grandTotal);
     }
 
+    @Test
+    void grandTotalOfSingleFile(@TempDir Path tmpDir) throws IOException {
+        Path rosFile = tmpDir.resolve("grandTotalOfSingleFile.txt");
+        Files.write(rosFile, "bread, 1, 2\n12-pack of eggs, 1, 3\n".getBytes());
+
+        int grandTotal = calculateGrandTotal(rosFile);
+
+        assertEquals(5, grandTotal, "grandTotal");
+    }
+
+    private int calculateGrandTotal(Path rosFile) throws IOException {
+        String rosLines = Files.readString(rosFile);
+        return calculateGrandTotal(rosLines);
+    }
+
     private int calculateGrandTotal(String rosLines) {
         int grandTotal = 0;
         String[] lines = rosLines.split("\n");
         for (String line : lines) {
+            // req 1) 1. we are a bit more generic by removing all white space
+            // req 1) 2. we do not want to add a record now, feels over-engineered.
             String[] recordOfSale = line.split("\\s*,\\s*");
             int total = Integer.parseInt(recordOfSale[2]);
             grandTotal += total;
