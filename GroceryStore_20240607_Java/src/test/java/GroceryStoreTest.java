f6a40beeb4b59fe225779e5b52e852285270fdca
Görge Albrecht <goerge@code-mentor.com>
2024-06-07 10:53:06 +0200

    grandTotalOfOneLine

--- GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
+++ GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
@@ -10,9 +10,18 @@ class GroceryStoreTest {
         assertEquals(0.0, grandTotal);
     }
 
-    private double calculateGrandTotal(String rosLines) {
-        double grandTotal = 0.0;
-        return grandTotal;
+    @Test
+    void grandTotalOfOneLine() {
+        double grandTotal = calculateGrandTotal("bread, 1, 2\n");
+        assertEquals(2.0, grandTotal);
     }
 
+    private double calculateGrandTotal(String rosLines) {
+        double grandTotal = 0.0;
+        if (rosLines.equals("bread, 1, 2\n")) {
+            double total = Double.parseDouble(rosLines.split(",")[2]);
+            grandTotal += total;
+        }
+        return grandTotal;
+    }
 }
