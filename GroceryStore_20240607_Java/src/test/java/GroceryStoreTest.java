1c8900821dcebc7a795aa84c6061763e656909b1
Peter Kofler <peter.kofler@code-cop.org>
2024-06-07 10:57:59 +0200

    Refactor back to int.

--- GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
+++ GroceryStore_20240607_Java/src/test/java/GroceryStoreTest.java
@@ -6,26 +6,26 @@ class GroceryStoreTest {
 
     @Test
     void grandTotalOfEmptyFile() {
-        double grandTotal = calculateGrandTotal("\n");
-        assertEquals(0.0, grandTotal);
+        int grandTotal = calculateGrandTotal("\n");
+        assertEquals(0, grandTotal);
     }
 
     @Test
     void grandTotalOfOneLine() {
-        double grandTotal = calculateGrandTotal("bread, 1, 2\n");
-        assertEquals(2.0, grandTotal);
+        int grandTotal = calculateGrandTotal("bread, 1, 2\n");
+        assertEquals(2, grandTotal);
     }
 
     @Test
     void grandTotalOfOneLineOfEggs() {
-        double grandTotal = calculateGrandTotal("12-pack of eggs, 1, 3\n");
-        assertEquals(3.0, grandTotal);
+        int grandTotal = calculateGrandTotal("12-pack of eggs, 1, 3\n");
+        assertEquals(3, grandTotal);
     }
 
-    private double calculateGrandTotal(String rosLines) {
-        double grandTotal = 0.0;
+    private int calculateGrandTotal(String rosLines) {
+        int grandTotal = 0;
         if (!rosLines.equals("\n")) {
-            double total = Double.parseDouble(rosLines.split(",")[2]);
+            int total = Integer.parseInt(rosLines.split(",")[2].trim());
             grandTotal += total;
         }
         return grandTotal;
