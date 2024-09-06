03d954b33f1df9693a0d639882e26ca6e44b2e2a
Peter Kofler <peter.kofler@code-cop.org>
2024-09-06 14:27:29 +0200

    Separate calculate one line from grand calculation.

--- GroceryStore_20240607_Java/src/main/java/Income.java
+++ GroceryStore_20240607_Java/src/main/java/Income.java
@@ -8,17 +8,22 @@ public class Income {
         String[] lines = rosLines.split("\n");
         for (int lineNumber = 0; lineNumber < lines.length; lineNumber++) {
             String line = lines[lineNumber];
-            // req 1) 1. we are a bit more generic by removing all white space
-            // req 1) 2. we do not want to add a record now, feels over-engineered.
-            try {
-                String[] recordOfSale = line.split("\\s*,\\s*");
-                int total = Integer.parseInt(recordOfSale[recordOfSale.length - 1]);
-                grandTotal += total;
-            } catch (Exception ex) {
-                throw new BadRosLineException(lineNumber + 1, line, ex);
-            }
+            grandTotal += calculateTotalOf(lineNumber, line);
         }
         return grandTotal;
+    }
+
+    private static int calculateTotalOf(int lineNumber, String line) {
+        try {
+
+            // req 1) 1. we are a bit more generic by removing all white space
+            // req 1) 2. we do not want to add a record now, feels over-engineered.
+            String[] recordOfSale = line.split("\\s*,\\s*");
+            return Integer.parseInt(recordOfSale[recordOfSale.length - 1]);
+
+        } catch (Exception ex) {
+            throw new BadRosLineException(lineNumber + 1, line, ex);
+        }
     }
 
     public int calculateGrandTotal(Path rosFile) {
