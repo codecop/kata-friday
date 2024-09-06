f2487fba4c5ee98eae12de25ce70de55c305f7bd
Peter Kofler <peter.kofler@code-cop.org>
2024-09-06 14:58:45 +0200

    Categorize all items using hardcoded map.

--- GroceryStore_20240607_Java/src/main/java/Categorizer.java
+++ GroceryStore_20240607_Java/src/main/java/Categorizer.java
@@ -7,6 +7,21 @@ public class Categorizer {
 
     public Categorizer() {
         categoryByProduct.put("bread", "wheat and pasta");
+        categoryByProduct.put("eggs", "animalic");
+        categoryByProduct.put("milk", "dairy");
+        categoryByProduct.put("coca cola", "sodas");
+        categoryByProduct.put("chicken", "meat");
+        categoryByProduct.put("beef", "meat");
+        categoryByProduct.put("carrots", "greens");
+        categoryByProduct.put("apples", "fruit");
+        categoryByProduct.put("butter", "dairy");
+        categoryByProduct.put("cheese", "dairy");
+        categoryByProduct.put("bacon", "meat");
+        categoryByProduct.put("juice", "drinks");
+        categoryByProduct.put("water", "drinks");
+        categoryByProduct.put("twixies", "candy");
+        categoryByProduct.put("tomatoes", "greens");
+        categoryByProduct.put("bananas", "fruit");
     }
 
     String categoryOf(String productName) {
