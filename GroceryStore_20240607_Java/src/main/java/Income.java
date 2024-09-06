1239b853747fb4b32ea09412506c078d48db13fc
Peter Kofler <peter.kofler@code-cop.org>
2024-09-06 13:45:06 +0200

    Parse ROS line regarding of existing comas in the text.

--- GroceryStore_20240607_Java/src/main/java/Income.java
+++ GroceryStore_20240607_Java/src/main/java/Income.java
@@ -10,7 +10,7 @@ public class Income {
             // req 1) 1. we are a bit more generic by removing all white space
             // req 1) 2. we do not want to add a record now, feels over-engineered.
             String[] recordOfSale = line.split("\\s*,\\s*");
-            int total = Integer.parseInt(recordOfSale[2]);
+            int total = Integer.parseInt(recordOfSale[recordOfSale.length - 1]);
             grandTotal += total;
         }
         return grandTotal;
--- GroceryStore_20240607_Java/src/test/java/IncomeTest.java
+++ GroceryStore_20240607_Java/src/test/java/IncomeTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -34,14 +32,12 @@ class IncomeTest {
 
     public static Stream<Arguments> grandTotalOfOneLineSource() {
         return Stream.of(
-                Arguments.of("bread, 1, 2\n", 2)
+                Arguments.of("bread, 1, 2\n", 2),
+                Arguments.of("apples (red, 1Kg bag), 1, 2\n", 2)
         );
     }
 
     // temporary test: grandTotalOfOneLineDifferentEntries
-
-    // TODO missing test for "apples (red, 1Kg bag), 1, 2"
-
 
     @Test
     void grandTotalOfMultipleLines() {
