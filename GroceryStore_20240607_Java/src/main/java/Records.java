61bce12636fb905128d6d630421ee176c586adb1
Görge Albrecht <goerge@code-mentor.com>
2024-10-04 15:09:44 +0200

    Calculate total per category

--- GroceryStore_20240607_Java/src/main/java/Records.java
+++ GroceryStore_20240607_Java/src/main/java/Records.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 record Records(List<RecordOfSale> entries) {
 
@@ -10,5 +12,16 @@ record Records(List<RecordOfSale> entries) {
             grandTotal += ros.total();
         }
         return grandTotal;
+    }
+
+    // req 2)
+
+    public Map<String, Integer> totalsPerCategory(Categorizer categorizer) {
+        Map<String, Integer> categoryTotals = new HashMap<>();
+        for (RecordOfSale ros : entries()) {
+            String category = categorizer.categoryOf(ros.item());
+            categoryTotals.put(category, categoryTotals.getOrDefault(category, 0) + ros.total());
+        }
+        return categoryTotals;
     }
 }
--- GroceryStore_20240607_Java/src/test/java/RecordsTest.java
+++ GroceryStore_20240607_Java/src/test/java/RecordsTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,5 +24,19 @@ public class RecordsTest {
         int grandTotal = records.grandTotal();
 
         assertEquals(0, grandTotal);
+    }
+
+    // req 2)
+
+    @Test
+    void totalPerCategory() {
+        Records records = new Records(Arrays.asList(
+                new RecordOfSale("bread", 2), //
+                new RecordOfSale("bread", 3) //
+        ));
+
+        Map<String, Integer> totals = records.totalsPerCategory(new Categorizer());
+
+        assertEquals(5, totals.get("wheat and pasta"));
     }
 }
